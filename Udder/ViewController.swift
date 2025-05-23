c7bb8e65168b10029908ebc9f2a0ecb69e372e8d
mcbridect <gthb01@mcmayan.com>
2025-05-23 15:46:49 -0400

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by admin on 5/23/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
